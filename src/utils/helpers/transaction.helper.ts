c559a7c1a705938796a76332fbd8201f5b23a278
khatrisagar <khatrisagar404@gmail.com>
2023-06-09 22:14:35 +0530

    optimizing code

--- src/utils/helpers/transaction.helper.ts
+++ src/utils/helpers/transaction.helper.ts
@@ -3,10 +3,12 @@ import { transactionInterface } from "@/interfaces";
 import store from "@/store";
 
 export const setUserTransactionState = () => {
-    const userTransactions = getTransactionFromLocalStorage().filter(
-        (transaction: transactionInterface) =>
-            transaction.userId ===
-            store.getters["user/getLoggedInUserState"].loggedInUserId
-    );
-    store.dispatch("transaction/setTransactionsState", userTransactions);
+  const userTransactions = getTransactionFromLocalStorage().filter(
+    (transaction: transactionInterface) =>
+      transaction.users.includes(
+        store.getters["user/getLoggedInUserState"].loggedInUserId
+      )
+  );
+  console.log(getTransactionFromLocalStorage());
+  store.dispatch("transaction/setTransactionsState", userTransactions);
 };
